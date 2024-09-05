b1445e4a3d653e07aaaf1b96d882822a0f3ced6b
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2024-09-05 10:30:47 +0530

    project setup and Redis Configuration Done

---
+++ src/test/java/com/example/UberProjectLocationService/UberProjectLocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberProjectLocationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectLocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
